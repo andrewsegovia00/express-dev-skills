4eb47defe0e365fc7c0bb5e5bbc4b3773e1eb0f1
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-06-28 21:17:30 -0700

    Latest update in www, package.json, views directory

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,11 +1,17 @@
 const skills = require(`../models/skill`);
 
 function index(req, res) {
-    res.render(`skills/index`, skills.getAll);
+    res.render(`skills/index`, 
+    { 
+        skills: skills.getAll()
+    });
 }
 
-function show() {
-    res.render(`skills/show`, skills.getOne(id));
+function show(res, res) {
+    res.render(`skills/show`, 
+    {
+        skill: skills.getOne(id)
+    });
 }
 
 module.exports = {
