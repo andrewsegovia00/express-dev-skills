afb92c7c565c7b7ee0ab0e99b3bfa77aca6d7ce0
andrewsegovia00 <andrew.segovia4@gmail.com>
2023-06-28 22:00:04 -0700

    Fix bugs

--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,13 @@
 const skills = [
-    {id: '12334', name: 'javscript', learned: true},
-    {id: '23445', name: 'express', learned: true},
-    {id: '34452', name: 'mongoDB', learned: false},
-    {id: '45212', name: 'node.js', learned: true},
-    {id: '92845', name: 'python', learned: false},
-    {id: '01924', name: 'postresSQL', learned: false},
-    {id: '19345', name: 'html', learned: true},
-    {id: '64821', name: 'css', learned: true},
-]
+    {id: 12334, name: 'javscript', learned: true},
+    {id: 23445, name: 'express', learned: true},
+    {id: 34452, name: 'mongoDB', learned: false},
+    {id: 45212, name: 'node.js', learned: true},
+    {id: 92845, name: 'python', learned: false},
+    {id: 11924, name: 'postresSQL', learned: false},
+    {id: 19345, name: 'html', learned: true},
+    {id: 64821, name: 'css', learned: true},
+];
 
 function getAll() {
     return skills;
